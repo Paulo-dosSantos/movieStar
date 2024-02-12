46d9119458224e0d2f9eb1effc55348d7c1d3727
Paulo Henrique <paulo.13.henrique800@gmail.com>
2024-02-12 16:30:59 -0300

    separação por templates

---
+++ templates/footer.php
@@ -0,0 +1,32 @@
+<footer id="footer">
+    <div id="social-container">
+      <ul>
+        <li>
+          <a href="" class="fab fa-facebook-square"></a>
+        </li>
+        <li>
+          <a href="" class="fab fa-instagram"></a>
+        </li>
+        <li>
+          <a href="" class="fab fa-youtube"></a>
+        </li>
+      </ul>
+    </div>
+
+    <div id="footer-links-container">
+      <ul>
+        <li> <a href="">Adicionar filmes</a> </li>
+        <li> <a href="">Adicionar crítica</a> </li>
+        <li> <a href="">Entrar/Registrar</a> </li>
+      </ul>
+    </div>
+    <p>&copy; 2020 Hora de codar</p>
+   </footer>
+
+
+
+     <!-- BOOTSTRAP JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous"></script>
+</body>
+</html>
